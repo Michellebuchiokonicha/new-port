ac2ce34438858ae525641e919f758fb656c7a980
michellebuchiokonicha <michelleokonicha.com>
2023-05-21 00:23:59 +0100

    u

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
